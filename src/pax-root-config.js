fb18f1bf2047647ddce85741b316369d31df2358
Diogo Perez <perezdiogo1@gmail.com>
2024-01-29 15:26:24 -0400

    ajuste de rotas e versao do react e react-dom

--- src/pax-root-config.js
+++ src/pax-root-config.js
@@ -2,15 +2,29 @@ import { registerApplication, start } from "single-spa";
 
 registerApplication({
   name: "@pax/pax-login",
-  app: () => System.import("@pax/pax-login"),
-  activeWhen: ["/pax-primavera"]
+  app: () => import("@pax/pax-login"),
+  activeWhen: (location) => location.pathname === '/login',
+  //activeWhen: ["/pax-primavera"]
+});
+
+registerApplication({
+  name: "@pax/pax-home",
+  app: () => import("@pax/pax-home"),
+  activeWhen: ["/pax-primavera"],
+});
+
+registerApplication({
+  name: "@pax/pax-associado",
+  app: () => import("@pax/pax-associado"),
+  //activeWhen: (location) => location.pathname === '/pax-primavera',
+  activeWhen: ["/associado"],
 });
 
 // registerApplication({
 //   name: "@pax/pax-associado",
 //   app: () => System.import("@pax/pax-associado"),
-//   // activeWhen: ["/pax-primavera/home"]
-//   activeWhen: (location) => location.pathname.startsWith('/pax-primavera/associado'),
+//   activeWhen: ["/associado"]
+//   // activeWhen: (location) => location.pathname.startsWith('/pax-primavera/associado'),
 // });
 
 start({
