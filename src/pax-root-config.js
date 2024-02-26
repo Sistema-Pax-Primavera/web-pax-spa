d7d31e5453b3c51e9929aa3f80361ce53cf7cb1f
Diogo Perez <perezdiogo1@gmail.com>
2024-02-26 09:04:52 -0400

    ajuste do arquivo root para leitura de json local com as definições de rota para cada aplicação

--- src/pax-root-config.js
+++ src/pax-root-config.js
@@ -1,37 +1,31 @@
 import { registerApplication, start } from "single-spa";
 import jsonData from '../src/aplications.json';
 
-fetch('https://run.mocky.io/v3/d2c61dab-6892-434c-8dac-22aabf9c42cb')
-  .then(resp => resp.json())
-  .then(data => {
-    data.applications.forEach(app => {
-      if (app.name === "@pax/pax-login") {
-        // Tratamento especial para a aplicação de login
-        registerApplication({
-          name: app.name,
-          app: () => System.import(app.package),
-          activeWhen: (location) => {
-            if (location.pathname === '/') {
-              window.location.href = '/login';
-              return false;
-            }
-            return location.pathname === '/login';
-          }
-        });
-      } else {
-        // Registro normal para outras aplicações
-        registerApplication({
-          name: app.name,
-          app: () => System.import(app.package),
-          activeWhen: app.exact
-            ? (location) => location.pathname === app.activeWhen
-            : [app.activeWhen],
-        });
+jsonData.applications.forEach(app => {
+  if (app.name === "@pax/pax-login") {
+    // Tratamento especial para a aplicação de login
+    registerApplication({
+      name: app.name,
+      app: () => System.import(app.package),
+      activeWhen: (location) => {
+        if (location.pathname === '/') {
+          window.location.href = '/login';
+          return false;
+        }
+        return location.pathname === '/login';
       }
     });
-  })
-  .finally(() => {
-    start({
-      urlRerouteOnly: true,
+  } else {
+    // Registro normal para outras aplicações
+    registerApplication({
+      name: app.name,
+      app: () => System.import(app.package),
+      activeWhen: app.exact
+        ? (location) => location.pathname === app.activeWhen
+        : [app.activeWhen],
     });
-  });
+  }
+});
+start({
+  urlRerouteOnly: true,
+});
